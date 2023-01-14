4879af5a5be32c2eb8c1ccb8758a1ef6de6879d7
siddhant-afk <siddhantdudhane21@gmail.com>
2023-01-14 17:12:17 +0530

    Validate new users

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,8 @@
 from market import app
-from flask import render_template
-from .models import Item
+from flask import render_template,redirect,url_for
+from .models import Item,User
 from .forms import RegisterForm
+from market import db
 
 @app.route("/")
 def home_page():
@@ -14,7 +15,15 @@ def market_page():
     return render_template("market.html",items=items)
 
 
-@app.route('/register')
+@app.route('/register',methods=["GET","POST"])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        new_user = User(username = form.username.data,
+        email_address = form.email_address.data,
+        password_hash = form.password1.data)
+
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('market_page'))
     return  render_template('register.html',form=form)
